222e919f7ebcfc06262923effb086c31a5d488d0
Erik Wyatt <wyatte@stolaf.edu>
2013-10-18 16:07:56 -0500

    form groups of the wrong size

--- src/main/scala/ppdm/main.scala
+++ src/main/scala/ppdm/main.scala
@@ -1,14 +1,17 @@
 package ppdm;
 
 import akka.actor._
+import akka.pattern._
 import akka.util.Timeout
 
 import scala.concurrent.duration._
-import scala.util.Random
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util._
 import collection._
 
 object Constants {
-  implicit val timeout = Timeout(5 second)
+  implicit val timeout = Timeout(10 second)
   val random = new Random()
   val modulus = 100
   val groupSize = 10
@@ -16,17 +19,27 @@ object Constants {
 import Constants._
 
 case class Finished()
-case class SetGroup(group:List[ActorRef])
+case class SetGroup(group:mutable.Set[ActorRef])
 case class GetSecret()
 case class SecureSum(job:Int)
 case class Key(key:Int, job:Int)
 case class Debug()
+case class GetDegree()
+case class AddNeighbor(neighbor:ActorRef)
+case class StartYourOwnGroup(stableRef:ActorRef)
+case class JoinMe(group:mutable.Set[ActorRef], groupID:Int, stableRef:ActorRef)
+case class GetGroup()
+case class Gossip(group:mutable.Set[ActorRef], groupID:Int, debug:Boolean)
+case class GroupingIsDone()
+case class Start()
+case class GetGroupID()
 
 object Node {
   def apply = {
-    val secret = random.nextInt(modulus / groupSize)
+    //In reality, we should be choosing moduli based on secrets and not the other way around
+    val secret = random.nextInt(modulus / (groupSize * 2))
     val jobs = new mutable.HashMap[Int, TempDataForSumming]
-    new Node(secret, Nil, jobs, false)
+    new Node(secret, mutable.HashSet[ActorRef](), None, jobs, false, mutable.HashSet[ActorRef](), None, 0, false)
   }
   def spawn(name:String, system:ActorSystem) = {system.actorOf(Props(Node.apply), name = name)}
 }
@@ -35,9 +48,14 @@ case class TempDataForSumming(var sum:Int, var nKeys:Int, var asker:Option[Actor
 
 case class Node (
              val secret:Int,
-             var group:List[ActorRef],
+             var group:mutable.Set[ActorRef],
+             var groupID:Option[Int],
              var jobs:mutable.Map[Int, TempDataForSumming],
-             var debug:Boolean
+             var debug:Boolean,
+             var neighbors:mutable.Set[ActorRef],
+             var parent:Option[ActorRef],
+             var unfinishedChildren:Int,
+             var finishedHere:Boolean
              ) extends Actor {
   def jobsString = {
     if (jobs isEmpty)
@@ -45,14 +63,139 @@ case class Node (
     else
       jobs map {x => "\t" + x._1.toString + "   " + x._2.toString + "\n"} reduce {_ + _}
   }
+
+  def GroupingFinished = {
+    if (unfinishedChildren == 0)
+      parent match {
+        case Some(pRef) => pRef ! GroupingIsDone
+        case None => println("GroupingFinished: parent is None")
+      }
+    else
+      finishedHere = true
+  }
+
+  def inviteFrom(outsiders:mutable.Set[ActorRef]): Unit = {
+    if (debug)
+      println("inviteFrom was called")
+    if (group.size < groupSize) {
+      if (debug)
+        println("Inviting")
+      groupID match {
+        case Some(groupIDInt) =>
+          outsiders headOption match {
+            case Some(outsider) => outsider ? JoinMe(group + self, groupIDInt, self) onComplete {
+              case Success(_) =>
+                group add outsider
+                unfinishedChildren += 1
+                inviteFrom(outsiders.tail)
+              case Failure(_) =>
+                inviteFrom(outsiders.tail)
+            }
+            case None => GroupingFinished
+          }
+        case None => println("InviteFrom: no group id")
+      }
+    } else {
+      if (debug)
+        println("Rejecting")
+      Future.traverse(outsiders)({outsider =>
+        outsider ? StartYourOwnGroup(self) andThen {
+          case Success(_) =>
+            unfinishedChildren +=1
+          case Failure(_) => Unit
+        }
+      }) onComplete {case _ => GroupingFinished}
+    }
+  }
+
   def receive = {
+    case GetGroupID =>
+      sender ! (groupID, self)
+    case Start =>
+      if (debug)
+        println("Start")
+      self ! StartYourOwnGroup(sender)
+    case Finished =>
+      if (debug)
+        println("Finished")
+    case GetGroup =>
+      if (debug)
+        println("GetGroup")
+      sender ! group + self
+    case GroupingIsDone =>
+      if (debug)
+        println("GroupingIsDone")
+      unfinishedChildren -= 1
+      if (finishedHere)
+        GroupingFinished
+    case StartYourOwnGroup(stableRef) =>
+        if (debug)
+          println("StartYourOwnGroup")
+        parent match {
+          case Some(_) => sender ! Status.Failure(new Exception)
+          case None => {
+            parent = Some(stableRef)
+            groupID = Some(random.nextInt())
+            sender ! Finished
+            inviteFrom(neighbors - stableRef)
+          }
+
+        }
+    case JoinMe(newGroup, newGroupID, stableRef) =>
+      if (debug)
+        println("JoinMe")
+      parent match {
+        case Some(_) => sender ! Status.Failure(new Exception)
+        case None => {
+          parent = Some(stableRef)
+          group = newGroup
+          groupID = Some(newGroupID)
+          sender ! Finished
+          for (member <- group)
+            member ! Gossip(group + self, newGroupID, debug)
+          inviteFrom(neighbors -- group)
+        }
+      }
+    case Gossip(similarGroup, otherGroupID, otherDebug) =>
+      if (false)
+        println("Gossip " + group.size.toString)// + "\n" + similarGroup.toString + "\n" + (group + self).toString)
+      groupID match {
+        case Some(groupIDInt) =>
+          if (groupIDInt == otherGroupID) {
+            if (!(similarGroup subsetOf group + self)) {
+              group ++= similarGroup - self
+              if (debug)
+                println("Gossiping " + group.size.toString)//self.toString + "\n" + group.size.toString + "\n" + similarGroup.toString + "\n" + (group + self).toString)
+              for (member <- group)
+                member ! Gossip(group + self, groupIDInt, debug)
+            } else {
+              if (false)
+                println("Not gossiping")
+            }
+          } else {
+             if (debug)
+              println("Gossip: wrong group ID")
+          }
+        case None =>
+          println("Gossip: no group ID")
+      }
+
+    case GetDegree =>
+      if (debug)
+        println("GetDegree")
+      sender ! neighbors.size
+    case AddNeighbor(neighbor) =>
+      neighbors add neighbor
+      if (debug)
+        println("AddNeighbor, degree is " + neighbors.size.toString)
+      sender ! Finished
     case Debug =>
       println("Debug enabled")
       debug = true
       sender ! Finished
     case GetSecret =>
-      if(debug)
-        println("GetSecret")
+      if (debug)
+        println("GetSecret " + secret.toString)
       sender ! secret
     case SetGroup(newGroup) =>
       if(debug)
@@ -73,7 +216,7 @@ case class Node (
       }
       jobs(job).asker = Some(sender)
       //maybe all the keys came in before we received the SecureSum message
-      if (jobs(job).nKeys == group.length) {
+      if (jobs(job).nKeys == group.size) {
         sender ! jobs(job).sum
         if (debug)
           println("SecureSum Finished\n" + jobsString)
@@ -90,7 +233,7 @@ case class Node (
       jobs(job).nKeys += 1
       jobs(job).asker match { //have we already gotten a SecureSum message?
         case Some(ref) =>
-          if (jobs(job).nKeys == group.length) {//have we collected all the keys?
+          if (jobs(job).nKeys == group.size) {//have we collected all the keys?
             ref ! jobs(job).sum
             if (debug)
               println("Key Finished\n" + jobsString)
@@ -99,7 +242,7 @@ case class Node (
       }
       if (debug)
         println("Key After\n" + jobsString)
-    case _ =>
-      sender ! Status.Failure(new Exception("Unknown message"))
+    case anything =>
+      sender ! Status.Failure(new Exception("Unknown message: " + anything.toString))
   }
 }
--- src/test/scala/unitTests.scala
+++ src/test/scala/unitTests.scala
@@ -6,6 +6,7 @@ import akka.pattern._
 import scala.concurrent.{ExecutionContext, Await, Future}
 import scala.concurrent.duration._
 import ExecutionContext.Implicits.global
+import collection._
 
 import org.scalatest.FlatSpec
 import Constants._
@@ -14,7 +15,7 @@ class PPDMSpec extends FlatSpec {
   def Group = new {
     val system = ActorSystem("ppdm")
     val nodes = for(i <- 0 until groupSize) yield Node.spawn("node" + i.toString, system)
-    Await.ready(Future.traverse(nodes)({node => node ? SetGroup(nodes.toList)}), 1 second)
+    Await.ready(Future.traverse(nodes)({node => node ? SetGroup(mutable.Set(nodes.toSeq: _*))}), 1 second)
   }
 
   "A group" should "securely compute totals" in {
@@ -40,5 +41,52 @@ class PPDMSpec extends FlatSpec {
     val sums = Await.result(Future.sequence(futures), 5 second)
     assert(sums.head === sums.last)
     group.system.shutdown()
+  }
+
+  def ERgraph(size:Int, targetDegree:Float) = new {
+    val system = ActorSystem("ppdm")
+    val nodes = for(i <- 0 until size) yield Node.spawn("node" + i.toString, system)
+    //Await.ready(nodes.head ? Debug, 1 second)
+    val linkP = (targetDegree / size) / 2
+    def cartesianProduct[A, B](left:List[A], right:List[B]) = {left flatMap {leftElem => right map {rightElem => (leftElem, rightElem)}}}
+    val noSelfLoops = cartesianProduct(nodes.toList, nodes.toList) filter (pair => pair._1 != pair._2)
+    val pairs = noSelfLoops filter (pair => random.nextFloat() < linkP)
+    def doubleLink(pair:(ActorRef, ActorRef)) = {Future.sequence((pair._1 ? AddNeighbor(pair._2)) :: (pair._2 ? AddNeighbor(pair._1)) :: Nil)}
+    Await.ready(Future.traverse(pairs)(doubleLink), 1 second)
+  }
+
+  "An Erdos-Renyi Random Graph" should "have the size and degree we asked for" in {
+    val size = 100
+    val degree = 5
+    val graph = ERgraph(size, degree)
+    val tolerance = .7
+    assert(graph.nodes.length == size)
+    val totalDeg = Future.reduce(graph.nodes map (node => (node ? GetDegree).mapTo[Int]))(_ + _)
+    val avgDeg = Await.result(totalDeg, 1 second).asInstanceOf[Float] / size
+    //println(avgDeg)
+    assert(degree * tolerance < avgDeg)
+    assert(avgDeg < degree / tolerance)
+    graph.system.shutdown()
+  }
+
+
+  it should "form groups" in {
+    val graph = ERgraph(1000, 5)
+    Await.ready(graph.nodes.head ? Debug, 1 second)
+    Await.ready(graph.nodes.head ? Start, 30 seconds)
+    //println(Await.result(Future.traverse(graph.nodes)(_ ? GetGroupID), 1 second).asInstanceOf[Vector[Int]])
+    val redundantGroups = Await.result(Future.traverse(graph.nodes)(_ ? GetGroup), 5 second)
+    val groups = redundantGroups.asInstanceOf[Vector[mutable.Set[ActorRef]]].distinct
+    println(groups map {_ size})
+    val nodesFromGroups = groups flatMap {elem => elem}
+    val distinctNodes = nodesFromGroups.distinct
+    assert(distinctNodes.length == nodesFromGroups.length, "Nodes are in at most one group")
+    assert(distinctNodes.length == graph.nodes.length, "Nodes are in at least one group")
+    val tolerance = .7
+    groups map {_.size} foreach {length =>
+      assert(tolerance * groupSize < length)
+      assert(length < groupSize / tolerance)
+    }
+    graph.system.shutdown()
   }
 }
