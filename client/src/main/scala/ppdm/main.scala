98ec0f0106bf0c67fc8d4b2b07f6f9c3bcbd8da4
Erik Wyatt <wyatte@stolaf.edu>
2013-11-22 17:06:14 -0600

    remoting on loopback

--- client/src/main/scala/ppdm/main.scala
+++ client/src/main/scala/ppdm/main.scala
@@ -1,3 +1,20 @@
+package ppdm;
+
+import akka.actor._
+
+import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Await, Future}
+import ExecutionContext.Implicits.global
+
+import ppdm.Constants._
+import NewAskPattern.ask
+
 object Main extends App {
-  println("client says hi")
+  val system = ActorSystem("client")
+  val secret = for {
+    remote <- system.actorSelection("akka.tcp://daemon@127.0.1.1:9963/user/node").resolveOne(1 second)
+    secret <- remote ? GetSecret()
+  } yield secret
+  println(Await.result(secret, 1 second))
+  system.shutdown()
 }
--- daemon/src/main/scala/ppdm/main.scala
+++ daemon/src/main/scala/ppdm/main.scala
@@ -1,3 +1,16 @@
+package ppdm;
+
+import akka.actor._
+
+import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Await, Future}
+import ExecutionContext.Implicits.global
+
+import ppdm.Constants._
+import NewAskPattern.ask
+
 object Main extends App {
-  println("daemon says hi")
+  val system = ActorSystem("daemon")
+  Node.spawn("node", system)
+  Thread.currentThread().interrupt()
 }
