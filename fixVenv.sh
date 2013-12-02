152626f8efcf2a550eb0c42f19bb4b0a59e679a2
Erik Wyatt <wyatte@stolaf.edu>
2013-12-02 16:19:36 -0600

    fix typo in fixVenv.sh

--- fixVenv.sh
+++ fixVenv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i s:VIRTUAL_ENV=\.\*:VIRTUAL_ENV=\"`pwd`/virtualenv\": virtualenv/bin/python
+sed -i s:VIRTUAL_ENV=\.\*:VIRTUAL_ENV=\"`pwd`/virtualenv\": virtualenv/bin/activate
 sed -i 1s:^.*$:#!`pwd`/virtualenv/bin/python: virtualenv/bin/fab
