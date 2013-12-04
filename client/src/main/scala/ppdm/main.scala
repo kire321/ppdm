d2a892800bcefeb01cfd93a2cbfaf57f51b02c5e
Erik Wyatt <wyatte@stolaf.edu>
2013-12-04 14:20:36 -0600

    test changes to client

--- client/src/main/scala/ppdm/main.scala
+++ client/src/main/scala/ppdm/main.scala
@@ -14,31 +14,42 @@ import java.net.NetworkInterface
 
 
 object Main extends App {
-  val e=NetworkInterface.getNetworkInterfaces();
-  while(e.hasMoreElements())
-  {
-    val n = e.nextElement();
-    val ee = n.getInetAddresses();
-    while(ee.hasMoreElements())
-    {
-      val i= ee.nextElement();
-      println(i.getHostAddress());
+
+  def getPublicIp = {
+    val e=NetworkInterface.getNetworkInterfaces();
+    while(e.hasMoreElements()) {
+      val n = e.nextElement();
+      val ee = n.getInetAddresses();
+      while(ee.hasMoreElements()) {
+        val i= ee.nextElement();
+      }
     }
-    println()
+
+
+    val addresses = enumerationAsScalaIterator(NetworkInterface.getNetworkInterfaces) flatMap {
+      i:NetworkInterface => enumerationAsScalaIterator(i.getInetAddresses)}
+    val ips = addresses map {_.getHostAddress} filter {str:String => str.count((c:Char) => c.toString == ".") == 3}
+    ips.next()
   }
 
 
-  val addresses = enumerationAsScalaIterator(NetworkInterface.getNetworkInterfaces) flatMap {
-    i:NetworkInterface => enumerationAsScalaIterator(i.getInetAddresses)}
-  val ips = addresses map {_.getHostAddress} filter {str:String => str.count((c:Char) => c.toString == ".") == 3}
   val system = ActorSystem("client")
-  val ip = ips.next()
-  println(ip)
-  val remoteName ="akka.tcp://daemon@" + ip + ":9963/user/node"
-  val secret = for {
+  val ip = getPublicIp
+  println(s"Looking for daemon at $ip")
+  println("Summing securely; will timeout in 60 seconds")
+  val remoteName = s"akka.tcp://daemon@$ip:9963/user/node"
+  val sum = for {
     remote <- system.actorSelection(remoteName).resolveOne(1 second)
-    secret <- remote ? GetSecret()
-  } yield secret
-  println(Await.result(secret, 1 second))
+    grouping <- PatientAsk(remote, Start(), system)
+    secureMap <- PatientAsk(remote, TreeSum(), system).mapTo[ActorMap]
+    printedReportedBack = println(shortGroup(secureMap.keys.reduce(_ ++ _)))
+    //Secure summing sometimes fails for unknown reasons, so this voting hack results in the correct total being selected
+    secureGroupSums = secureMap.values map {sums =>
+      sums.groupBy(x => x).maxBy((pair:(Int, List[Int])) => pair._2.length)._1
+    }
+    secureSum = secureGroupSums.fold(0)(_ + _)
+  } yield secureSum
+  Await.result(sum, 60 seconds)
+  println(s"Total is $sum")
   system.shutdown()
 }
--- daemon/src/main/scala/ppdm/main.scala
+++ daemon/src/main/scala/ppdm/main.scala
@@ -5,12 +5,26 @@ import akka.actor._
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Await, Future}
 import ExecutionContext.Implicits.global
+import collection._
 
 import ppdm.Constants._
 import NewAskPattern.ask
 
+
+class mockNode extends Actor {
+  def receive = {
+
+    case Start() =>
+      sender ! Finished
+
+    case TreeSum() =>
+      sender ! (immutable.HashMap((immutable.HashSet(self), List(0))):ActorMap)
+  }
+}
+
 object Main extends App {
   val system = ActorSystem("daemon")
-  Node.spawn("node", system)
+  system.actorOf(Props(new mockNode), name = "node")
+  //Node.spawn("node", system)
   Thread.currentThread().interrupt()
 }
