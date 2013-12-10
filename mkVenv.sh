1db3d980b03288574378ba3e1700eccec5316488
Erik F Wyatt <wyatte@rns202-3.cs.stolaf.edu>
2013-12-10 15:51:16 -0600

    fast

--- mkVenv.sh
+++ mkVenv.sh
@@ -3,6 +3,6 @@ set -e
 
 curl -O https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.10.tar.gz
 tar xvfz virtualenv-1.10.tar.gz
-python virtualenv-1.10/virtualenv.py venv
+/usr/bin/python virtualenv-1.10/virtualenv.py venv
 source venv/bin/activate
 pip install pexpect
